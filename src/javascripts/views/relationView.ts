e59f1536b9931e1ef9e7387805f8a150eea82e37
HoangHongNgo <honghoangngo@gmail.com>
2023-08-14 17:46:05 +0700

    feat: add relation view

---
+++ src/javascripts/views/relationView.ts
@@ -0,0 +1,57 @@
+import Relation from "../models/relation";
+import RelationTmpl from "../templates/relationTmpl";
+
+class RelationView {
+  relationListEl: HTMLElement;
+  relationDropDownEl: HTMLElement;
+
+  /**
+   * Constructor of RelationView object
+   */
+  constructor() {
+    this.relationListEl = document.querySelector(".relation-list")!;
+    this.relationDropDownEl = document.querySelector(".relation-dropdown")!;
+  }
+
+  //----- RENDERING -----//
+
+  /**
+   * Render the relation list in adding or editing modal.
+   * @param {Relation[]} relations
+   */
+  renderRelationList = (relations: Relation[]): void => {
+    relations.forEach((relation) => {
+      this.renderRelation(relation);
+    });
+  };
+
+  /**
+   * Render the relation list in filter dropdown.
+   * @param {Relation[]} relations
+   */
+  renderRelationDropdownList = (relations: Relation[]): void => {
+    relations.forEach((relation) => {
+      this.renderRelationDropdown(relation);
+    });
+  };
+
+  /**
+   * Render a relation in relation list in adding or editing modal.
+   * @param {Relation} relation
+   */
+  renderRelation = (relation: Relation): void => {
+    const relationTemplate: string = RelationTmpl.renderRelationOpt(relation);
+    this.relationListEl.innerHTML += relationTemplate;
+  };
+
+  /**
+   * Render a relation in relation list in filter dropdown.
+   * @param {Relation} relation
+   */
+  renderRelationDropdown = (relation: Relation): void => {
+    const relationDropDownTemplate: string = RelationTmpl.renderRelationDropDown(relation);
+    this.relationDropDownEl.innerHTML += relationDropDownTemplate;
+  };
+}
+
+export default RelationView;
